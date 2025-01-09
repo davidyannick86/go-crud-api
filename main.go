1cd97e5140f44dac327e1fb7cc21d6f2789226ad
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-01-09 18:15:40 +0100

    add year field to Movie struct and implement getMovies handler with mock data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,6 +13,7 @@ type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
 	Title    string    `json:"title"`
+	Year     string    `json:"year"`
 	Director *Director `json:"director"`
 }
 
@@ -22,8 +24,15 @@ type Director struct {
 
 var movies []Movie
 
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
 func main() {
 	r := mux.NewRouter()
+
+	movies = getMockDatas()
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
@@ -33,4 +42,31 @@ func main() {
 
 	fmt.Println("Server is running on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func getMockDatas() []Movie {
+	movies = append([]Movie{}, Movie{
+		ID:       "1",
+		Isbn:     "438227",
+		Title:    "The Godfather",
+		Year:     "1972",
+		Director: &Director{Firstname: "Francis Ford", Lastname: "Coppola"},
+	})
+
+	movies = append(movies, Movie{
+		ID:       "2",
+		Isbn:     "454545",
+		Title:    "Goodfellas",
+		Year:     "1990",
+		Director: &Director{Firstname: "Martin", Lastname: "Scorsese"},
+	})
+
+	movies = append(movies, Movie{
+		ID:       "3",
+		Isbn:     "454545",
+		Title:    "The Shawshank Redemption",
+		Year:     "1994",
+		Director: &Director{Firstname: "Frank", Lastname: "Darabont"},
+	})
+	return movies
 }
